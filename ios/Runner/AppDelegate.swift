631d6f68fd797f1a5753429cd8bde9b8f5051080
Alexandre Roux <alex@tekartik.com>
2019-03-08 02:36:13 +0100

    fix: sqflite in isolate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,12 +11,15 @@ import sqflite
     GeneratedPluginRegistrant.register(with: self)
     
     let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-    let backgroundEngine = FlutterEngine(name: "BackgroundIsolate", project: nil, allowHeadlessExecution: true)
     let methodChannel = FlutterMethodChannel(name: "com.example.sqflite/backgrounded", binaryMessenger: controller)
     
     methodChannel.setMethodCallHandler { call, result in
         let array = call.arguments as! Array<Any>
         let handle = array[0] as! Int64
+        
+        // Create it here to avoid registration issue the second time
+        // Not sure if that's correct though
+        let backgroundEngine = FlutterEngine(name: "BackgroundIsolate", project: nil, allowHeadlessExecution: true)
         
         let callbackInformation = FlutterCallbackCache.lookupCallbackInformation(handle)
         backgroundEngine!.run(withEntrypoint: callbackInformation?.callbackName, libraryURI: callbackInformation?.callbackLibraryPath)
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sqflite_bug_sample/main.dart';
+import 'package:sqflitebackgroundbugsample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
